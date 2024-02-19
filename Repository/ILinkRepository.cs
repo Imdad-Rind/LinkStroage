e31f5fec38b35f528e638f2341c907d1b93793da
Imdad Rind <aimdad79@hotmail.com>
2024-02-19 21:23:54 +0500

    Updates : Created Link Repository and Service

---
+++ Repository/ILinkRepository.cs
@@ -0,0 +1,14 @@
+using LinkStorage.Models;
+
+namespace LinkStorage.Repository;
+
+public interface ILinkRepository
+{
+    Task CreateLink(Links links);
+    Task<Links> GetLinkById(Guid id);
+    Task<IEnumerable<Links>> GetAllLinks();
+    Task DeleteLinkById(Guid id);
+    Task<Links> UpdateLink(Links link);
+
+
+}
