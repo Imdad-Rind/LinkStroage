410b256f02d511b6b907598f9ddd212fccebd86d
Imdad Rind <aimdad79@hotmail.com>
2024-03-15 12:30:15 +0500

    Update : Now there is Counter for publicPost, Followers and Following

---
+++ Migrations/20240315071636_NowThereIsCouctForPostAndFolloers.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LinkStorage.Migrations
+{
+    /// <inheritdoc />
+    public partial class NowThereIsCouctForPostAndFolloers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "FollowersCount",
+                table: "Users_Table",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<long>(
+                name: "FollowingsCount",
+                table: "Users_Table",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<long>(
+                name: "PublicPostCount",
+                table: "Users_Table",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FollowersCount",
+                table: "Users_Table");
+
+            migrationBuilder.DropColumn(
+                name: "FollowingsCount",
+                table: "Users_Table");
+
+            migrationBuilder.DropColumn(
+                name: "PublicPostCount",
+                table: "Users_Table");
+        }
+    }
+}
